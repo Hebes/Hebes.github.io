a9a9ca8c794ea40ecc2ee960ff3e2887e82fad74
Hebes <1607388033@qq.com>
2024-02-01 08:30:08 +0800

    快捷打开应用程序.bat

---
+++ 快捷打开应用程序.bat
@@ -0,0 +1,18 @@
+@echo off
+chcp 65001
+title 快捷打开应用程序
+color bf
+
+start "" "D:/Program Files/PixPin/PixPin.exe"
+TIMEOUT /T 0
+
+start "" "D:\Program Files\zz_v2rayN-With-Core-SelfContained\v2rayN.exe"
+TIMEOUT /T 0
+
+start "" "D:\Program Files\Tencent\WeChat\WeChat.exe"
+TIMEOUT /T 0
+
+start "" "D:\Program Files (x86)\NetEase\CloudMusic\cloudmusic.exe"
+TIMEOUT /T 0
+
+start "" "D:\Program Files\Unity Hub\Unity Hub.exe"
